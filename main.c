b35baa516934905f98c9410d5af12681d2c35860
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-08 17:46:06 -0300

    feat: add basic test of the linked list implementation

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include"linked_list.h"
 
 int main() {
+    t_list *list = create_list();
+
+    append(list, 1);
+    append(list, 4);
+    append(list, 9);
+    append(list, 20);
+
+    print_list(list);
+
+    destroy_list(list);
 
     return 0;
 }
