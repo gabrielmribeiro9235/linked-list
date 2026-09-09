16224fe36f4ce18d03d50d69bbd2e9c9257939f4
gabrielmribeiro9235 <meloribeiro0757@gmail.com>
2026-09-09 09:45:29 -0300

    feat: create menu function in main file

--- main.c
+++ main.c
@@ -2,6 +2,33 @@
 #include<stdlib.h>
 #include"linked_list.h"
 
+void menu() {
+    printf("-----------------------------------------------\n");
+    printf("Select a function:\n");
+    printf(" 1 - print_list\n");
+    printf(" 2 - append\n");
+    printf(" 3 - insert\n");
+    printf(" 4 - sort\n");
+    printf(" 5 - remove_item\n");
+    printf(" 6 - remove_at\n");
+    printf(" 7 - find\n");
+    printf(" 8 - get_node_by_item\n");
+    printf(" 9 - get_node_by_index\n");
+    printf("10 - size\n");
+    printf("11 - is_empty\n");
+    printf("12 - clear\n");
+    printf("13 - peek_front\n");
+    printf("14 - peek_back\n");
+    printf("15 - pop_front\n");
+    printf("16 - pop_back\n");
+    printf("17 - reverse\n");
+    printf("18 - merge\n");
+    printf("19 - clone\n");
+    printf("20 - exit\n");
+    printf("-----------------------------------------------\n");
+    printf("Your choice: ");
+}
+
 int main() {
     t_list *list = create_list();
 
